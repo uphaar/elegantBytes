5b142b9da65c650a741e52539977f9c420d58630
uphaar <goyaluphaar@gmail.com>
2017-07-25 18:47:07 -0500

    Add files via upload

---
+++ app/scripts/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('elegantBytesApp', ['ui.router','ngResource'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/main.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
+                }
+
+            })
+        
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'                  
+                    }
+                }
+            })
+        
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'                  
+                    }
+                }
+            })
+
+            // route for the menu page
+          
+    
+        $urlRouterProvider.otherwise('/');
+    })
+;
